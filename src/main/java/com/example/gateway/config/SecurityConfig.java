793b73c796e5ea1daeed6c54a38f5665870cfd11
isreactive <alexandru.mireuta@stud.acs.upb.ro>
2024-01-14 18:55:04 +0200

    Few changes

--- src/main/java/com/example/gateway/config/SecurityConfig.java
+++ src/main/java/com/example/gateway/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.gateway.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -25,13 +26,15 @@ import org.springframework.security.web.server.authentication.ServerAuthenticati
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.security.web.server.util.matcher.OrServerWebExchangeMatcher;
 import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.client.RestTemplate;
+
 
 @Configuration
 @RequiredArgsConstructor
 class SecurityConfig {
     final ReactiveClientRegistrationRepository reactiveClientRegistrationRepository;
+
+    @Value("${server.url}")
+    public String serverUrl;
 
     @Bean
     @Order(0)
@@ -62,13 +65,13 @@ class SecurityConfig {
     public ServerLogoutSuccessHandler oidcLogoutHandler() {
         OidcClientInitiatedServerLogoutSuccessHandler successHandler =
                 new OidcClientInitiatedServerLogoutSuccessHandler(reactiveClientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri("http://localhost:8080");
+        successHandler.setPostLogoutRedirectUri(serverUrl);
         return successHandler;
     }
 
     @Bean
     public ServerAuthenticationFailureHandler oidcAuthenticationFailureHandler() {
-        return new RedirectServerAuthenticationFailureHandler("http://localhost:8085");
+        return new RedirectServerAuthenticationFailureHandler(serverUrl);
     }
 
     @Bean
